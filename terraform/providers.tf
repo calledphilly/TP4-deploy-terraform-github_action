1eece371565358f8eb7fa10660b399f480f36dcb
calledphilly <ybikouta@icloud.com>
2025-07-23 21:17:21 +0200

    Comment out Terraform backend configuration in providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,9 +1,9 @@
-terraform {
+# terraform {
   
-  backend "s3" {
-    bucket         = "terraform-backend-terraformbackends3bucket-kmf8fwf9q4zr"
-    key            = "terraform.tfstate"
-    region         = "eu-west-1"
-    dynamodb_table = "terraform-backend-TerraformBackendDynamoDBTable-1H2S07OFDUIEQ"
-  }
-}
+#   backend "s3" {
+#     bucket         = "terraform-backend-terraformbackends3bucket-kmf8fwf9q4zr"
+#     key            = "terraform.tfstate"
+#     region         = "eu-west-1"
+#     dynamodb_table = "terraform-backend-TerraformBackendDynamoDBTable-1H2S07OFDUIEQ"
+#   }
+# }
